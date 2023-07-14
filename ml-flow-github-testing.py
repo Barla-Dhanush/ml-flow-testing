0f46b5f623df73cbc567503209f1245c7bed2152
dhanudhanush2998@gmail.com <dhanudhanush2998@gmail.com>
2023-07-14 15:16:33 +0000

    added ml-flow-github-testing

---
+++ ml-flow-github-testing.py
@@ -0,0 +1,48 @@
+# Databricks notebook source
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+
+db = load_diabetes()
+X = db.data
+y = db.target
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# COMMAND ----------
+
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+# COMMAND ----------
+
+experiment_name = "/Shared/mlflow-github-testing/"
+mlflow.set_experiment(experiment_name)
+
+with mlflow.start_run():
+  n_estimators = 100
+  max_depth = 6
+  max_features = 3
+  # Create and train model
+  rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
+  rf.fit(X_train, y_train)
+  # Make predictions
+  predictions = rf.predict(X_test)
+  
+  # Log parameters
+  mlflow.log_param("num_trees", n_estimators)
+  mlflow.log_param("maxdepth", max_depth)
+  mlflow.log_param("max_feat", max_features)
+  
+  # Log model
+  mlflow.sklearn.log_model(rf, "random-forest-model")
+  
+  # Create metrics
+  mse = mean_squared_error(y_test, predictions)
+    
+  # Log metrics
+  mlflow.log_metric("mse", mse)
+
+# COMMAND ----------
+
+
