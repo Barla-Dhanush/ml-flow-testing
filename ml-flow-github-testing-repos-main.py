aa23aff7d74cdd56c207219ebeea49d0eb9c5983
dhanudhanush2998@gmail.com <dhanudhanush2998@gmail.com>
2023-07-18 14:25:52 +0000

    commit

--- ml-flow-github-testing-repos-main.py
+++ ml-flow-github-testing-repos-main.py
@@ -16,6 +16,32 @@ from sklearn.metrics import mean_squared_error
 
 # COMMAND ----------
 
+with mlflow.start_run():
+  n_estimators = 100
+  max_depth = 6
+  max_features = 3
+  # Create and train model
+  rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
+  rf.fit(X_train, y_train)
+  # Make predictions
+  predictions = rf.predict(X_test)
+  
+  # Log parameters
+  mlflow.log_param("num_trees", n_estimators)
+  mlflow.log_param("maxdepth", max_depth)
+  mlflow.log_param("max_feat", max_features)
+  
+  # Log model
+  mlflow.sklearn.log_model(rf, "random-forest-model")
+  
+  # Create metrics
+  mse = mean_squared_error(y_test, predictions)
+    
+  # Log metrics
+  mlflow.log_metric("mse", mse)
+
+# COMMAND ----------
+
 experiment_name = "/Shared/mlflow-github-testing/"
 mlflow.set_experiment(experiment_name)
 
